6052efaf72bf209e1106f752e1bd9e2978eefb51
Akshay Oommen <Akshay.Oommen@accolite.com>
2019-01-16 13:39:59 +0530

    Added navigation between recipe and shopping-list pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-project';
+  title: string = 'my-project';
+  loadedOption: string = 'recipe';
+
+  onNavigate(option: string) {
+    this.loadedOption = option;
+  }
 }
