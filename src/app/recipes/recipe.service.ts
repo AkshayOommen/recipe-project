9e255871f09112a01bad9c32f5ec88a94af6de48
Akshay Oommen <axeabraham@hotmail.com>
2019-01-21 19:56:21 +0530

    Updated ShoppingListService recipe and added ingredients to Recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { Recipe } from './recipe-list/recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('A sample recipe', 'A sample description', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-        new Recipe('Another sample recipe', 'Another sample description', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+        new Recipe(
+            'A sample recipe', 
+            'A sample description', 
+            'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png', 
+            [
+                new Ingredient('Bananas', 10),
+                new Ingredient('Apples', 5)
+            ]),
+        new Recipe(
+            'Another sample recipe', 
+            'Another sample description', 
+            'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png', 
+            [
+                new Ingredient('Bread', 10),
+                new Ingredient('Chicken', 15)
+            ])
     ]
 
     getRecipes() {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
-    
+    ingredientAdded = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 10),
+        new Ingredient('Tomato', 5)
+    ]
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    onIngredientAdded(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientAdded.emit(this.ingredients.slice());
+    }    
 }
